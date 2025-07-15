68b47c5e0bba134775dbe40cf289f3af9d2a572b
Sébastien Gervilla <sebastiengervilla.c@gmail.com>
2025-07-15 20:13:01 +0200

    imp: Added isAdministrator to useAuthentication hook

--- src/hooks/useAuthentication/useAuthentication.ts
+++ src/hooks/useAuthentication/useAuthentication.ts
@@ -3,10 +3,18 @@ import {
     AuthenticationContextProps,
     RouteType,
 } from '@/contexts/AuthenticationContext';
+import { TalksService } from '@/services/talks-service';
 import { useContext } from 'react';
 
 const useAuthentication = <T extends RouteType>() => {
-    return useContext<AuthenticationContextProps<T>>(AuthenticationContext);
+    const context = useContext<AuthenticationContextProps<T>>(AuthenticationContext);
+
+    const isAdministrator = context.user && context.user.role === TalksService.Models.User.Role.Administrator;
+
+    return {
+        ...context,
+        isAdministrator,
+    }
 }
 
 export default useAuthentication;
