c02c67e872e0a10a03fd01c4ac6fdacdae290649
Sébastien Gervilla <sebastiengervilla.c@gmail.com>
2025-07-15 20:18:28 +0200

    feat: Added rooms constants

---
+++ src/constants/conferences.ts
@@ -0,0 +1,14 @@
+import { TalksService } from "@/services/talks-service";
+
+export const rooms = [
+    TalksService.Models.Conference.Room.RoomA,
+    TalksService.Models.Conference.Room.RoomB,
+    TalksService.Models.Conference.Room.RoomC,
+    TalksService.Models.Conference.Room.RoomD,
+    TalksService.Models.Conference.Room.RoomE,
+    TalksService.Models.Conference.Room.RoomF,
+    TalksService.Models.Conference.Room.RoomG,
+    TalksService.Models.Conference.Room.RoomH,
+    TalksService.Models.Conference.Room.RoomI,
+    TalksService.Models.Conference.Room.RoomJ,
+];
